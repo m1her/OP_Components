6f541f8a0ffe76163cef011a7bc2d67ee8556c3d
m1her <maher.ple2@gmail.com>
2025-02-11 21:56:44 +0200

    drag kinda done

--- src/components/CustomizableNavigation/EditWebNavLinks/DraggableLink.tsx
+++ src/components/CustomizableNavigation/EditWebNavLinks/DraggableLink.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { useDrag, useDrop } from "react-dnd";
-import type { Identifier, XYCoord } from "dnd-core";
-import { ItemTypes, NavLinkType } from "../data";
+import type { XYCoord } from "dnd-core";
+import { NavLinkType } from "../data";
 
 interface DragItem {
   index: number;
@@ -20,85 +20,50 @@ export const DraggableLink: React.FC<DraggableLinkProps> = ({
   moveLink,
   index,
 }) => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  const [{ handlerId }, drop] = useDrop<
-    DragItem,
-    void,
-    { handlerId: Identifier | null }
-  >({
-    accept: ItemTypes.WEBLINK,
-    collect(monitor) {
-      return {
-        handlerId: monitor.getHandlerId(),
-      };
-    },
-    hover(item: DragItem, monitor) {
+  const [, dropRef] = useDrop({
+    accept: "item",
+    hover: (item: DragItem, monitor) => {
       if (!ref.current) {
         return;
       }
       const dragIndex = item.index;
       const hoverIndex = index;
 
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        
-        return;
-      }
+      if (dragIndex === hoverIndex) return;
 
-      // Determine rectangle on screen
       const hoverBoundingRect = ref.current?.getBoundingClientRect();
 
-      // Get vertical middle
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const hoverMiddleX =
+        (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
 
-      // Determine mouse position
       const clientOffset = monitor.getClientOffset();
+      const hoverActualX = (clientOffset as XYCoord).x - hoverBoundingRect.left;
 
-      // Get pixels to the top
-      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+      if (dragIndex < hoverIndex && hoverActualX < hoverMiddleX) return;
+      if (dragIndex > hoverIndex && hoverActualX > hoverMiddleX) return;
 
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return;
-      }
-
-      // Time to actually perform the action
       moveLink(dragIndex, hoverIndex);
 
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
       item.index = hoverIndex;
     },
   });
-  const [{ isDragging }, drag] = useDrag({
-    type: ItemTypes.WEBLINK,
-    item: () => ({ id: linkData.id, index: linkData.index }),
+
+  const [{ isDragging }, dragRef] = useDrag({
+    type: "item",
+    item: { index },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
   });
 
-  drag(drop(ref));
+  const ref = useRef<HTMLDivElement>(null);
+  const dragDropRef = dragRef(dropRef(ref));
 
   return (
     <div
-      ref={ref}
-      data-handler-id={handlerId}
-      className={`select-none min-w-16 text-center rounded h-fit shadow-md bg-indigo-700 px-4 py-1 transition-opacity cursor-move
-        ${isDragging ? "opacity-20" : "opacity-100"}`}
+      ref={dragDropRef}
+      className="select-none min-w-20 text-center rounded h-fit shadow-md bg-indigo-700 px-4 py-1 transition-opacity cursor-move"
+      style={{ opacity: !isDragging ? 1 : 0.1 }}
     >
       {linkData.title}
     </div>
--- src/components/CustomizableNavigation/EditWebNavLinks/index.tsx
+++ src/components/CustomizableNavigation/EditWebNavLinks/index.tsx
@@ -1,46 +1,46 @@
 "use client";
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
-import { NavLinkType } from "../data";
+import React, { useCallback, useState } from "react";
+// import { NavLinkType } from "../data";
 import { DraggableLink } from "./DraggableLink";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
-import update from "immutability-helper";
+// import update from "immutability-helper";
 
 export const EditWebNavLinks = () => {
   const [links, setLinks] = useState([
-    { id: "1", index: 1, title: "Home", link: "/" },
-    { id: "2", index: 2, title: "About", link: "/" },
-    { id: "3", index: 3, title: "Services", link: "/" },
-    { id: "4", index: 4, title: "Contact", link: "/" },
-    { id: "5", index: 5, title: "Blog", link: "/" },
-    { id: "6", index: 6, title: "Help", link: "/" },
-    { id: "7", index: 7, title: "Profile", link: "/" },
+    { id: 1, index: 1, title: "Home", link: "/" },
+    { id: 2, index: 2, title: "About", link: "/" },
+    { id: 3, index: 3, title: "Services", link: "/" },
+    { id: 4, index: 4, title: "Contact", link: "/" },
+    { id: 5, index: 5, title: "Blog", link: "/" },
+    { id: 6, index: 6, title: "Help", link: "/" },
+    { id: 7, index: 7, title: "Profile", link: "/" },
   ]);
 
-  const moveLink = (dragIndex: number, hoverIndex: number) => {
-    console.log(dragIndex, hoverIndex);
-    
-    setLinks((prevCards: NavLinkType[]) =>
-      update(prevCards, {
-        $splice: [
-          [dragIndex, 1],
-          [hoverIndex, 0, prevCards[dragIndex] as NavLinkType],
-        ],
-      })
-    );
-  };
+  const moveLink = useCallback((dragIndex: number, hoverIndex: number) => {
+        const dragItem = links[dragIndex]
+        const hoverItem = links[hoverIndex]
+        setLinks(links => {
+            const updatedPets = [...links]
+            updatedPets[dragIndex] = hoverItem
+            updatedPets[hoverIndex] = dragItem
+            return updatedPets
+        })
+    },
+    [links],
+)
 
   return (
     <DndProvider backend={HTML5Backend}>
       <ul className="">
         <FontAwesomeIcon icon={faGear} className="w-4 h-4 cursor-pointer" />
         <div className="fixed z-50 top-0 left-0 inset-0 bg-black/60 backdrop-blur-[2px] flex justify-center items-center">
-          <div className="p-8 rounded-md bg-zinc-700 flex flex-col gap-6 w-full h-full items-center justify-center">
+          <div className="p-8 rounded-md bg-zinc-700 flex gap-6 w-full h-full items-center justify-center">
             {links.map((navLink, idx) => (
               <DraggableLink
-                key={idx}
+                key={navLink.id}
                 index={idx}
                 linkData={navLink}
                 moveLink={moveLink}
